e8b96df88f5975de6dd3dced209eefce8775019a
Asmaa Elsobahy <asmaaelsopahy@gmail.com>
2024-05-08 18:20:01 +0300

    add Login

--- backend/users/views.py
+++ backend/users/views.py
@@ -21,6 +21,18 @@ class SignUpView(GenericAPIView):
             return Response(data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+# class LoginView(GenericAPIView):
+#     permission_classes = (AllowAny,)
+#     serializer_class = UserLoginSerializer
+
+#     def post(self, request):
+#         email = request.data.get('email')
+#         password = request.data.get('password')
+#         user = authenticate(email=email, password=password)
+#         if user:
+#             token = RefreshToken.for_user(user)
+#             return Response({'refresh': str(token), 'access': str(token.access_token)})
+#         return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
 class LoginView(GenericAPIView):
     permission_classes = (AllowAny,)
     serializer_class = UserLoginSerializer
@@ -28,10 +40,23 @@ class LoginView(GenericAPIView):
     def post(self, request):
         email = request.data.get('email')
         password = request.data.get('password')
+        print('='*10)
+        print(request)
+        print(request.data)
+        print(f'\nemail={email}\npassword={password}')
         user = authenticate(email=email, password=password)
+        print(f'\nuser={user}')
         if user:
+            print('User authenticated successfully\n')
+            print('='*10)
             token = RefreshToken.for_user(user)
-            return Response({'refresh': str(token), 'access': str(token.access_token)})
+            data = {
+                'message': 'Login successful',
+                'tokens': {'refresh': str(token), 'access': str(token.access_token)}
+            }
+            return Response(data)
+        print('User authenticated Failled\n')
+        print('='*10)
         return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
 
 class LogoutView(GenericAPIView):
