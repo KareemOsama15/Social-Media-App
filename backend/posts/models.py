4e09798f68aaae9b8d0b048e442a94d031790854
AyaTarek95 <aya.tarek213@icloud.com>
2024-05-15 22:21:03 +0300

    Revert "PostsListView"
    This reverts commit d36bff33e96efa4e813c81b97e6d1c93135ee543.

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -13,8 +13,8 @@ class Post(models.Model):
         return f"{self.author.username} - {self.created_at}"
 
 """class Connection(models.Model):
-    from_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='following')
-    to_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='followers')
+    from_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+    to_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
 
     class Meta:
         unique_together = (('from_user', 'to_user'),)
--- backend/posts/views.py
+++ backend/posts/views.py
@@ -1,34 +1,3 @@
 from django.shortcuts import render
-from dhango.views import View
-from .models import Post
-from .forms import PostForm
 
-
-class PostListView(View):
-    """Lists all posts of current user"""
-    def get(self, request, *args, **kwargs):
-        posts = Post.objects.all().order_by('-created_at')
-        form = PostForm()
-
-        context = {
-            'post_list': posts,
-            'form': form,
-        }
-
-        return render(request, 'posts/post_list.html', context)
-
-    def post(self, request, *args, **kwargs):
-        posts = Post.objects.all().order_by('-created_at')
-        form = PostForm(request.POST)
-
-        if form.is_valid():
-            new_post = form.save(commit=Flase)
-            new_post.author = request.user # get the current loggedin user
-            new_post.save()
-
-        context = {
-            'post_list': posts,
-            'form': form,
-        }
-
-        return render(request, 'posts/post_list.html', context)
+# Create your views here.
