9b794b52f6a3ff6e41f4151ed309ea08f04d014e
KareemOsama15 <karim.osama.saleh@gmail.com>
2024-05-20 23:01:29 +0300

    fix indetation in comments serializer

--- backend/comments/serializers.py
+++ backend/comments/serializers.py
@@ -3,8 +3,8 @@ from .models import Comment
 from users.models import CustomUser
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.CharField(source='self.request.user',read_only=True)
+	author = serializers.CharField(source='self.request.user',read_only=True)
 
-    class Meta:
-	model = Comment
-	fields = '__all__'
+	class Meta:
+		model = Comment
+		fields = '__all__'
